0231bf48dde843e914814a52cc33dd092e675b52
Satoshi Innami <innamisatoshi@gmail.com>
2019-01-09 21:54:42 +0900

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-toolbar
+      fixed
+      app
+      color="teal lighten-3"
+    >
+    <v-toolbar-title v-text="title"/>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: true,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Simple Chat'
+      }
+    }
+  }
+</script>
